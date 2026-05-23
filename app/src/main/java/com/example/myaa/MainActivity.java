134075ecbc744dac948dc21e6a29e783a4a42c39
Mandrein1313 <kk0830582814@gmail.com>
2026-05-23 10:13:18 +0700

    🚀 Build from MiniStudio - 2026-05-23 10:13:18

---
+++ app/src/main/java/com/example/myaa/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.myaa;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.myaa.R;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
